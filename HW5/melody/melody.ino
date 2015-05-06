f8ce737809a6a27d2f89e0f26077e1b6568f0a8b
Mendelson9 <Mendelson9@gmail.com>
2015-05-05 17:54:15 -0700

    HW5 finished
    Space Harrier theme song played on an arduino board

---
+++ HW5/melody/melody.ino
@@ -0,0 +1,42 @@
+
+#include "pitches.h"
+
+// notes in the melody:
+int melody[] = {
+  nF5, nE5, nE5, nG4, nA4, nF5, nE5, nE5, nNO, nF5, nE5, nE5,nD5, nE5, nF5, nGS5, nG5, nF5 };
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4,8,2,4,4,4,8,2,8,4,8,2,4,4,1,3,4,4 };
+
+void setup() {
+}
+
+// song is designed to loop
+void loop() {
+// iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 18; thisNote++) {
+    //18 = how many notes will play in melody[]
+    
+    // when the same note is played twice in a row, insert an extra small delay so you can tell when the end/beggining are.
+    if (melody[thisNote] == melody[thisNote + 1]) {
+     delay(100);
+  } 
+
+    // to calculate the note duration, take one second
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(8, melody[thisNote],noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    
+
+    
+    noTone(8);
+  }
+}
