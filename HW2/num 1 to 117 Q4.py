5a976d694f66165375d9faa7700db2a1c75f044a
Mendelson9 <Mendelson9@gmail.com>
2015-05-12 11:36:14 -0700

    minor edits

--- HW2/num 1 to 117 Q4.py
+++ HW2/num 1 to 117 Q4.py
@@ -1,4 +1,4 @@
-#range function creates an array from 1 to 117
+#range function creates an array of integers from 1 to 117
 num0to117 = range(1,118)
 
 #sum will add all the numbers in the num0to117 array
