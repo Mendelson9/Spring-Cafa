39475a4e00360a92d3c6e5cd5451f9f03e8612d8
Mendelson9 <Mendelson9@gmail.com>
2015-05-12 11:34:51 -0700

    HW2 Q4 finished
    summing integer 1 to 117

---
+++ HW2/num 1 to 117 Q4.py
@@ -0,0 +1,9 @@
+#range function creates an array from 1 to 117
+num0to117 = range(1,118)
+
+#sum will add all the numbers in the num0to117 array
+final = sum(num0to117)
+
+#print the final answer
+print "When you sum the integers from 1 to 117, you get "
+print final
